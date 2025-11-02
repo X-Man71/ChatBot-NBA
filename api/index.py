20ced73e86c3f1c8ba36f291c7d23551aef22ffa
X-Man71 <tnono0816@icloud.com>
2025-11-02 15:07:47 +0900

    hello

--- api/index.py
+++ api/index.py
@@ -8,7 +8,6 @@ from service.kakaologin import get_user_info_from_kakao, kakao_login
 from nba_api.stats.static import players
 import pandas as pd
 import requests, random
-
 import json
 import service.player_rank as player_rank
 import service.search as search
---
+++ api/service/__init__.py
@@ -0,0 +1,11 @@
+# service/__init__.py
+
+# 전역 변수
+VERSION = "1.0"
+
+# 전역 함수
+def hello_service():
+    return "Hello from service package!"
+
+# 패키지 로딩 시 초기화 코드
+print("service 패키지가 로드되었습니다.")
